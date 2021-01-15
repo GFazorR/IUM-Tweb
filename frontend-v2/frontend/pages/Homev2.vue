9d57d15589427843b7a27b4039e2f85d651053f9
G. Russo <44084859+GFazorR@users.noreply.github.com>
2021-01-15 23:14:38 +0100

    New Homepage version added

--- frontend-v2/frontend/pages/Homev2.vue
+++ frontend-v2/frontend/pages/Homev2.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="index">
-    <b-progress :value="value" :max="max" animated></b-progress>
-    <b-card v-if="getSelectedSubject == null">
+    <b-card>
       <b-container fluid>
         <h2>Seleziona una materia</h2>
 
@@ -18,7 +17,7 @@
             <b-button
               block
               variant="info"
-              @click="setSubject(s), state()"
+              @click="setSubject(s)"
               style="width: 190px"
             >
               {{ s.name }}
@@ -29,15 +28,8 @@
       <!-- /Seleziona Materia -->
     </b-card>
 
-    <b-card v-if="this.getSelectedSubject != null && getSelectedSlot == null">
-      <b-button
-        class="ml-4 mb-2"
-        variant="danger"
-        @click="$store.commit('SET_SUBJECT', null), state()"
-      >
-        Back
-      </b-button>
-      <h3 class="ml-4">Seleziona lo slot per {{ getSelectedSubject.name }}</h3>
+    <b-card v-if="this.getSelectedSubject != null">
+      <h3 class="ml-4">Seleziona lo slot</h3>
       <b-container fluid>
         <b-row>
           <b-col
@@ -47,7 +39,7 @@
             :key="d.date"
             class="mx-auto"
           >
-            <p class="px-2 pb-1 mb-2 border-bottom border-dark text-center">
+            <p class="px-2 pb-1 mb-2 border-bottom border-info text-center">
               {{ $moment(d.date).format("ddd DD/MM/YY") }}
             </p>
             <div v-for="sl of d.daySlots" :key="sl.date">
@@ -55,7 +47,7 @@
                 class="w-100 mb-1"
                 :disabled="!sl.isAvailable"
                 :variant="available(sl)"
-                @click="setSlot(sl), state()"
+                @click="setSlot(sl)"
               >
                 {{ $moment(sl.date).format("HH:mm") }}
               </b-button>
@@ -64,10 +56,7 @@
         </b-row>
       </b-container>
     </b-card>
-    <b-card v-if="getSelectedSlot != null && getSelectedTeacher == null">
-      <b-button variant="danger" @click="setSlot(null), state()">
-        Back
-      </b-button>
+    <b-card v-if="getSelectedSlot != null">
       <h3>Seleziona Professore</h3>
       <b-container fluid>
         <b-row>
@@ -78,7 +67,11 @@
             v-for="t of getSelectedSlot.teachers"
             :key="t.id"
           >
-            <b-button @click="setTeacher(t), state()" style="width: 200px;">
+            <b-button
+              variant="info"
+              @click="setTeacher(t)"
+              style="width: 200px;"
+            >
               {{ t.name }}
             </b-button>
           </b-col>
@@ -86,9 +79,6 @@
       </b-container>
     </b-card>
     <b-card v-if="getSelectedTeacher != null">
-      <b-button variant="danger" @click="setTeacher(null), state()">
-        Back
-      </b-button>
       <h3>Riepilogo Prenotazione</h3>
       <p><b>Materia: </b>{{ getSelectedSubject.name }}</p>
       <p>
@@ -97,11 +87,7 @@
       </p>
       <p><b>Professore: </b>{{ getSelectedTeacher.name }}</p>
       <div class="mx-auto mt-5" style="width: 200px;">
-        <b-button
-          variant="success"
-          @click="bookSlot(), (value = 0)"
-          style="width: 200px;"
-        >
+        <b-button variant="success" @click="bookSlot()" style="width: 200px;">
           Prenota!
         </b-button>
       </div>
@@ -120,10 +106,7 @@ export default {
     return { subjects };
   },
   data() {
-    return {
-      value: 0,
-      max: 100
-    };
+    return {};
   },
   computed: {
     ...mapGetters([
@@ -146,37 +129,7 @@ export default {
       return !s.isAvailable //|| this.$moment(s.date).isBefore(this.$moment.now())
         ? "danger"
         : "success";
-    },
-    state() {
-      if (
-        this.getSelectedSubject == null &&
-        this.getSelectedSlot == null &&
-        this.getSelectedTeacher == null
-      ) {
-        this.value = 0;
-      } else if (
-        this.getSelectedSubject != null &&
-        this.getSelectedSlot == null &&
-        this.getSelectedTeacher == null
-      ) {
-        this.value = 33;
-      } else if (
-        this.getSelectedSubject != null &&
-        this.getSelectedSlot != null &&
-        this.getSelectedTeacher == null
-      ) {
-        this.value = 66;
-      } else if (
-        this.getSelectedSubject != null &&
-        this.getSelectedSlot != null &&
-        this.getSelectedTeacher != null
-      ) {
-        this.value = 100;
-      }
     }
-  },
-  beforeMount() {
-    this.state();
   }
 };
 </script>
